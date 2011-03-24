b034be528401348c70915b40f33c792d630dd6e9
Leandro López (inkel) <inkel.ar@gmail.com>
2011-03-23 21:41:12 -0300

    Helper to construct 3FUNKYMONKEYS as in the logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def threefunkymonkeys
+    content_tag(:span, :class => "brand") do
+      raw(
+          returning("") do |brand|
+            brand << content_tag(:span) do
+              "3"
+            end
+            brand << content_tag(:span, :class => "funky") do
+              "funky"
+            end
+            brand << content_tag(:span) do
+              "monkeys"
+            end
+          end
+          )
+    end
+  end
 end
