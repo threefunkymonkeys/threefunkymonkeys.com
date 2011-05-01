0b38e31e3ff7a75a81465c7bd88c7eabfc88b45f
Leandro López (inkel) <inkel.ar@gmail.com>
2011-05-01 17:44:40 -0300

    Fix wrong error message on duplicated email

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,13 +4,17 @@ class ContactsController < ApplicationController
 
     respond_to do |format|
       if @contact.save
-        format.html do 
-          flash[:notice] = t('application.messages.thank_you') 
+        format.html do
+          flash[:notice] = t('application.messages.thank_you')
           redirect_to root_path
         end
       else
         format.html do
-          flash[:error] = t('application.messages.invalid_email')
+          if Contact.find_by_email(@contact.email)
+            flash[:warning] = t('application.messages.duplicated_email')
+          else
+            flash[:error] = t('application.messages.invalid_email')
+          end
           redirect_to root_path
         end
       end
