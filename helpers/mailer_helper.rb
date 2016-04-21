c722dec7f1ac6a4fa3ee74f5f6b6f937fa40ea09
Lautaro Orazi <orazile@gmail.com>
2016-04-21 08:59:20 -0300

    Replace Mandrill with Postmark

--- helpers/mailer_helper.rb
+++ helpers/mailer_helper.rb
@@ -1,3 +1,4 @@
+require "postmark"
 require "net/http"
 require "uri"
 require "json"
@@ -6,42 +7,26 @@ require 'base64'
 module ThreeFunkyMonkeys
   module MailerHelpers
     def self.send_contact(email)
-      request_body = {
-        :key => ENV["MANDRILL_API_KEY"],
-        :message => {
-          :to => [{
-                    "email" => "contact@threefunkymonkeys.com",
-                    "name" => "INFO 3FM",
-                    "type" => "to"
-                  }],
-          :from_name => email["name"],
-          :from_email => email["email"],
-          :subject => "Contact form: " + email["subject"],
-          :html => email["message"].gsub("\n", "<br>"),
-          :text => email["message"],
-          :headers => {},
-          :track_opens => true,
-          :track_clicks => true,
-          :auto_text => true,
-          :url_strip_qs => true,
-          :preserve_recipients => false,
-          :bcc_address => '',
-          :attachments => []
-        },
-        :async => false
+      client = Postmark::ApiClient.new(ENV["POSTMARK_API_TOKEN"])
+
+      html_body = <<-HTMLEMAIL
+        <p>Name: #{email["name"]} - #{email["email"]}</p>
+        <p>#{email["message"].gsub("\n", "<br>")}</p>
+      HTMLEMAIL
+
+      postmark_message = {
+        :from => "Administrador 3FM <admin@threefunkymonkeys.com>",
+        :to => "Info 3FM <contact@threefunkymonkeys.com>",
+        :subject => "Contact form: " + email["subject"],
+        :html_body => html_body,
+        :text_body => "#{email["name"]} - #{email["email"]}\n#{email["message"]}",
+        :track_opens => true
       }
 
-      uri = URI('https://mandrillapp.com')
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
+      response = client.deliver(postmark_message)
 
-      request = Net::HTTP::Post.new('/api/1.0/messages/send.json', initheader = {'Content-Type' =>'application/json'})
-      request.body = request_body.to_json
 
-      response = http.start {|http| http.request(request)}
-      puts response.body
-
-      return response.code.to_i == 200
+      return response[:message] == "OK"
     end
   end
 end
