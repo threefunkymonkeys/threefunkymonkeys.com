9589461a472043418125bdeb29cd4be217cb5eaa
Leonardo Mateo <leonardomateo@gmail.com>
2020-04-12 20:37:09 -0300

    Add term to spam banning expression

--- concerns/check_spam.rb
+++ concerns/check_spam.rb
@@ -11,7 +11,7 @@ module ThreeFunkyMonkeys
       def run
         url_regex = /(?:(?:https?|ftp):\/\/|\b(?:[a-z\d]+\.))(?:(?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))?\))+(?:\((?:[^\s()<>]+|(?:\(?:[^\s()<>]+\)))?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))?/
 
-        ban_regex = /(sexy|sex|girls|chicas|for the night|pasar la noche|порно|порнография|порнуха|порнушка|affiliate|marketing|bitcoin|investieren|tinyurl|Прокип|Андрей|продукт|объявлений|Размещение)/i
+        ban_regex = /(porn|sexy|sex|girls|chicas|for the night|pasar la noche|порно|порнография|порнуха|порнушка|affiliate|marketing|bitcoin|investieren|tinyurl|Прокип|Андрей|продукт|объявлений|Размещение)/i
 
         if ban_regex.match(@message) || url_regex.match(@message)
           return error({ message: ["not_valid"] })
